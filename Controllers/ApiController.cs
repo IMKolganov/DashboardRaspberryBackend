ad155bba6bc973ec309641d2a07a4f0e47a0cfd9
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2024-07-23 01:36:24 +0300

    added get-soil-moisture

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -7,15 +7,25 @@ namespace DashboardRaspberryBackend.Controllers;
 public class ApiController : BaseApiController
 {
     private readonly string _getTemperatureAndHumidifyUrl;
+    private readonly string _getSoilMoistureUrl;
+
 
     public ApiController(ILogger<ApiController> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)
         : base(logger, httpClientFactory, configuration)
     {
         _getTemperatureAndHumidifyUrl = configuration["MicroserviceSettings:GetTemperatureAndHumidifyUrl"];
+        _getSoilMoistureUrl = configuration["MicroserviceSettings:GetSoilMoistureUrl"];
 
         if (string.IsNullOrWhiteSpace(_getTemperatureAndHumidifyUrl))
         {
-            throw new ArgumentException("Microservice URL is not configured.");
+            throw new ArgumentException("Microservice GetTemperatureAndHumidifyUrl is not configured.");
+        }
+
+        _getSoilMoistureUrl = configuration["MicroserviceSettings:GetSoilMoistureUrl"];
+
+        if (string.IsNullOrWhiteSpace(_getSoilMoistureUrl))
+        {
+            throw new ArgumentException("Microservice GetSoilMoistureUrl is not configured.");
         }
     }
 
@@ -23,5 +33,11 @@ public class ApiController : BaseApiController
     public async Task<IActionResult> GetTemperatureAndHumidify()
     {
         return await CallExternalMicroserviceAsync(_getTemperatureAndHumidifyUrl + "/get-temperature-and-humidify");
+    }
+
+    [HttpGet("GetSoilMoisture")]
+    public async Task<IActionResult> GetSoilMoisture()
+    {
+        return await CallExternalMicroserviceAsync(_getSoilMoistureUrl + "/get-soil-moisture");
     }
 }
